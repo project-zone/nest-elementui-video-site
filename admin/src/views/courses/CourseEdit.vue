8644a814d77cfc2122cfc87c9c878fc4679fd7e3
ray <qray@foxmail.com>
2020-02-06 14:57:34 +0800

    handle create and edit api

--- admin/src/views/courses/CourseEdit.vue
+++ admin/src/views/courses/CourseEdit.vue
@@ -1,36 +1,55 @@
 <template>
   <div class="course-list">
-    <h3>{{pageTitle}}</h3>
-    <ele-form :form-data="data" :form-desc="fields"></ele-form>
+    <h3>{{ pageTitle }}</h3>
+    <ele-form
+      :form-data="data"
+      :form-desc="fields"
+      :request-fn="submit"
+    ></ele-form>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component({
   name: 'CourseEdit',
 })
-export default class CourseEdit  extends Vue {
-  @Prop(String) id!: string
+export default class CourseEdit extends Vue {
+  @Prop(String) id!: string;
 
   data = {}
 
   fields = {
-    name: { label: '课程名称', type: 'input'},
+    name: { label: '课程名称', type: 'input' },
     cover: { label: '课程封面图', type: 'input' },
+  };
+
+  get isNew() {
+    return !this.id
   }
 
   get pageTitle() {
-    return `${!this.id ? '创建': '编辑'}课程`
-  }
-
-  async fetch() {
-
+    return `${this.id ? '创建' : '编辑'}课程`;
   }
 
   created() {
-    // this.fetch()
+    !this.isNew && this.fetch()
+  }
+
+  async fetch() {
+    const { data } = await this.$http.get(`courses/${this.id}`)
+
+    this.data = data
+  }
+
+  async submit(data: any) {
+    const url = this.isNew ? 'courses' : `courses/${this.id}`
+    const method = this.isNew ? 'post' : 'put'
+
+    await this.$http[method](url, data);
+    this.data = {}
+    this.$router.go(-1)
   }
 
 }
--- admin/src/views/courses/CourseList.vue
+++ admin/src/views/courses/CourseList.vue
@@ -2,41 +2,45 @@
   <div class="course-list">
     <h3>课程列表</h3>
     <el-table :data="data.data" border stripe>
-      <el-table-column v-for="(field, key) in fields"
+      <el-table-column
+        v-for="(field, key) in fields"
         :prop="key"
         :key="key"
         :label="field.label"
-        :width="field.width">
+        :width="field.width"
+      >
       </el-table-column>
+      <el-table-column label="操作" :width="200" v-slot="scope">
+        <el-button size="small" type="primary" @click="$router.push(`/courses/edit/${ scope.row._id }`)">编辑</el-button>
+        <el-button size="small" type="primary">删除</el-button>
+         </el-table-column>
     </el-table>
-
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator';
 
 @Component({
   name: 'CourseList',
 })
-export default class CourseList  extends Vue {
-  data = []
+export default class CourseList extends Vue {
+  data = [];
 
   fields = {
-    _id: { label: 'ID'},
-    name: { label: '课程名称'},
+    _id: { label: 'ID' },
+    name: { label: '课程名称' },
     cover: { label: '课程封面图' },
-  }
+  };
 
   async fetch() {
-    const { data } = await this.$http.get('courses')
+    const { data } = await this.$http.get('courses');
 
-    this.data = data
+    this.data = data;
   }
 
   created() {
-    this.fetch()
+    this.fetch();
   }
-
 }
 </script>
