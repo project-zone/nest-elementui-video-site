e44830b28fac7a0eb3f5974371a4308b433bfe01
ray <qray@foxmail.com>
2020-02-07 16:08:26 +0800

    set vue env

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 Vue.config.devtools = true
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.VUE_APP_API_URL
 })
 
 Vue.prototype.$http = http
