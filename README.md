61c17b53a4dedd4539dbb87717ee31ef6df22d6b
ray <qray@foxmail.com>
2020-02-08 17:14:11 +0800

    modify the auth strategy name

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # nest-elementui-video-site
-### Using nest and elementUI to develop full stack.
+## Using nest.js and vue.js to develop full stack.
 
-Handbooks are:
-[nest.js](https://docs.nestjs.com/)
-[element ui](https://element.eleme.cn/#/zh-CN/component/installation)
+### Handbooks are:
+
+- [nest.js](https://docs.nestjs.com/)
+
+- [element ui](https://element.eleme.cn/#/zh-CN/component/installation)
+
+### Port
+all port defined in .env.example
+- Front web: `3100`
+- Front Admin: `8080`
+- Backend Admin: `3000`
+- Backend Server: `3001`
--- server/apps/server/src/auth/jwt.strategy.ts
+++ server/apps/server/src/auth/jwt.strategy.ts
@@ -3,10 +3,8 @@ import { Strategy, StrategyOptions, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectModel } from 'nestjs-typegoose';
 import { User } from '@libs/db/models/user.model';
-import { BadRequestException } from '@nestjs/common';
-import { compareSync } from 'bcryptjs';
 
-export class JwtStrategy extends PassportStrategy(Strategy, 'local') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   // 使用策略模式里面使用的名字，这里叫'local'，使用的时候也要用'local'
   constructor(
     @InjectModel(User) private readonly userModel: ReturnModelType<typeof User>,
